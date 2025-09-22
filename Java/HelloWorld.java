42dd2ce1a9046bdf2c332fe5ee57920aeb570ca8
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-09-22 20:57:43 +0800

    Add: beginner Java code

---
+++ Java/HelloWorld.java
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
